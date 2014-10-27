6ba8e040d848fea23cc23cbedcf393a311bba7da
Peeyush Prasad <prasad@astron.nl>
2014-10-27 21:19:39 +0000

    Commit from gpu02 with minor changes to some bash scripts.

--- src/check_stations.sh
+++ src/check_stations.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# declare -A port2station = ( ["53268"]="CS002" ["53276"]="CS003" ["53284"]="CS004" ["53292"]="CS005" ["53300"]="CS006" ["53308"]="CS007" );
-for port in 53268 53276 53284 53292 53300 53308; do
+declare -A port2station=( ["53268"]="CS002" ["53276"]="CS003" ["53284"]="CS004" ["53292"]="CS005" ["53300"]="CS006" ["53308"]="CS007" );
+# for port in 53268 53276 53284 53292 53300 53308; do
+for port in ${!port2station[@]}; do
 	# printf "\n\n--> Checking port $port, station ${port2station["$port"]}\n"
-	printf "\n\n--> Checking port $port.\n"
+	printf "\n\n--> Checking port $port, station ${port2station[$port]}.\n"
 	sudo timeout 2 tcpdump -vvxSnelfi eth2 -c 1 -s 100 udp port $port
+	# sudo  tcpdump -vvxSnelfi eth2 -c 1 -s 100 udp port $port & sleep 10s && pkill -HUP -f tcpdump 
+	
 done;
